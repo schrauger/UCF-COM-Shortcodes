834e9265ac43465b416e77d923893c99fea12d95
Stephen Schrauger <stephen@schrauger.com>
2014-10-13 15:57:24 -0400

    finished moving functions to class

--- functions.php
+++ functions.php
@@ -29,8 +29,67 @@ class ucf_com_shortcodes_settings {
 
 
 	public function __construct() {
+		register_activation_hook( __FILE__, array(
+			$this,
+			'on_activation'
+		) ); //call the 'on_activation' function when plugin is first activated
+		register_deactivation_hook( __FILE__, array(
+			$this,
+			'on_deactivation'
+		) ); //call the 'on_deactivation' function when plugin is deactivated
+		register_uninstall_hook( __FILE__, array(
+			$this,
+			'on_uninstall'
+		) ); //call the 'uninstall' function when plugin is uninstalled completely
+
+		// Register the 'settings' page
 		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
 		add_action( 'admin_init', array( $this, 'page_init' ) );
+
+		// Register the tinymce hooks to create buttons
+		add_filter( 'mce_external_plugins', array( $this, 'tinymce_brightcove_js' ) );
+		add_filter( 'mce_css', array( $this, 'tinymce_brightcove_css' ) );
+		add_filter( 'mce_buttons', array( $this, 'tinymce_brightcove_button' ) );
+	}
+
+	/**
+	 * Function that is run when the plugin is activated via the plugins page
+	 */
+	public function on_activation() {
+		// stub
+	}
+
+	public function on_deactivation() {
+		// stub
+	}
+
+	public function on_uninstall() {
+		// stub
+	}
+
+	/**
+	 * Calls all of the 'add_shortcode' api calls, after running duplication checks
+	 */
+	public function init_shortcodes() {
+		$this->_init_shortcodes( 'brightcove', array( $this, 'shortcode_brightcove_function' ) );
+		$this->_init_shortcodes( 'shortcode', array( $this, 'shortcode_base_url_function' ) );
+
+	}
+
+	/**
+	 * Adds a shortcode, but only if the shortcode isn't already defined. If it is,
+	 * simply skip that shortcode (and probably log the event so the user doesn't
+	 * get frustrated trying to figure out why the shortcode isn't working the way
+	 * they wanted).
+	 *
+	 * @param string   $tag  Shortcode tag to be searched in post content.
+	 * @param callable $func Hook to run when shortcode is found.
+	 */
+	private function _init_shortcodes( $tag, $func ) {
+		if ( ! ( shortcode_exists( $tag ) ) ) {
+			add_shortcode( $tag, func );
+		}
+
 	}
 
 	/**
@@ -59,7 +118,7 @@ class ucf_com_shortcodes_settings {
 		$this->baseurl_options    = get_option( self::baseurl_name );
 		?>
 		<div class="wrap" >
-			<?php screen_icon(); ?>
+
 			<h2 >My Settings</h2 >
 
 			<form method="post" action="options.php" >
@@ -107,6 +166,51 @@ class ucf_com_shortcodes_settings {
 			                                    'section' => self::brightcove_section
 			)
 		);
+		add_settings_field(
+			'brightcove_playerKey',
+			'PlayerKey',
+			array(
+				$this,
+				'shortcodes_input_text'
+			),   // The name of the function responsible for rendering the option interface
+			self::page_slug,                          // The page on which this option will be displayed
+			self::brightcove_section,         // The name of the section to which this field belongs
+			array(                              // The array of arguments to pass to the callback.
+			                                    'id'      => 'brightcove_playerKey', // copy/paste id here
+			                                    'label'   => 'PlayerKey as defined by your Brightcove account.',
+			                                    'section' => self::brightcove_section
+			)
+		);
+		add_settings_field(
+			'brightcove_default_height',
+			'Default height',
+			array(
+				$this,
+				'shortcodes_input_text'
+			),   // The name of the function responsible for rendering the option interface
+			self::page_slug,                          // The page on which this option will be displayed
+			self::brightcove_section,         // The name of the section to which this field belongs
+			array(                              // The array of arguments to pass to the callback.
+			                                    'id'      => 'brightcove_default_height', // copy/paste id here
+			                                    'label'   => 'Default video height (in pixels)',
+			                                    'section' => self::brightcove_section
+			)
+		);
+		add_settings_field(
+			'brightcove_default_width',
+			'Default width',
+			array(
+				$this,
+				'shortcodes_input_text'
+			),   // The name of the function responsible for rendering the option interface
+			self::page_slug,                          // The page on which this option will be displayed
+			self::brightcove_section,         // The name of the section to which this field belongs
+			array(                              // The array of arguments to pass to the callback.
+			                                    'id'      => 'brightcove_default_width', // copy/paste id here
+			                                    'label'   => 'Default video width (in pixels)',
+			                                    'section' => self::brightcove_section
+			)
+		);
 	}
 
 	public function brightcove_section_info() {
@@ -116,134 +220,26 @@ class ucf_com_shortcodes_settings {
 	public function shortcodes_input_text( $args ) {
 		// Note the ID and the name attribute of the element should match that of the ID in the call to add_settings_field
 		$html = '<input type="text" id="' . $args[ 'id' ] . '" name="' . $args[ 'section' ] . '[' . $args[ 'id' ] . ']" value="' . get_option( $args[ 'id' ] ) . '"/>';
-		// @TODO make sure the input is sanitized. it should be from the sanitize function on save, but probably
-		//  should check on display as well.
+		// @TODO make sure the input is sanitized. it should be from the sanitize function on save, but probably should check on display as well.
 
 		// Here, we will take the first argument of the array and add it to a label next to the input
 		$html .= '<label for="' . $args[ 'id' ] . '"> ' . $args[ 'label' ] . '</label>';
 		echo $html;
 	}
 
-}
-
-add_action( 'admin_init', 'ucf_com_shortcodes_settings' );
-function ucf_com_shortcodes_settings() {
-	$settings_page       = 'writing'; // display all of these settings on the 'writing' settings page
-	$brightcove_section  = 'brightcove_settings';
-	$promo_video_section = 'promo_video_settings';
-
 	/**
-	 * Brightcove Options
+	 * brightcove Short Code
+	 * Replaces the 'brightcove' keyword with the actual HTML desired and returns it to wordpress.
+	 * This places an inline video on the page. The video is hosted on Brightcove.com.
+	 *
+	 * @param array $attrs Attributes are passed by wordpress automatically.
+	 *
+	 * @return string
+	 *
 	 */
-	add_settings_section(
-		$brightcove_section,
-		'Custom Shortcode Options - Brightcove (brightcove)',
-		'brightcove_options_callback',
-		$settings_page
-	);
-	add_settings_field(
-		'ucf_com_shortcodes_brightcove_playerID',                      // ID used to identify the field throughout the theme
-		'PlayerID (deprecated)',                           // The label to the left of the option interface element
-		'ucf_com_shortcodes_input_text',   // The name of the function responsible for rendering the option interface
-		$settings_page,                          // The page on which this option will be displayed
-		$brightcove_section,         // The name of the section to which this field belongs
-		array(                              // The array of arguments to pass to the callback.
-		                                    'ucf_com_shortcodes_brightcove_playerID',
-		                                    'PlayerID as defined by your Brightcove account. This has been replaced by the playerKey field.'
-		)
-	);
-	register_setting(
-		$settings_page,
-		'ucf_com_shortcodes_brightcove_playerID'
-	);
-	add_settings_field(
-		'ucf_com_shortcodes_brightcove_playerKey',
-		'PlayerKey',
-		'ucf_com_shortcodes_input_text',
-		$settings_page,
-		$brightcove_section,
-		array(
-			'ucf_com_shortcodes_brightcove_playerKey',
-			'PlayerKey as defined by your Brightcove account'
-		)
-	);
-	register_setting(
-		$settings_page,
-		'ucf_com_shortcodes_brightcove_playerKey'
-	);
-	add_settings_field(
-		'ucf_com_shortcodes_brightcove_default_height',
-		'Default height',
-		'ucf_com_shortcodes_input_text',
-		$settings_page,
-		$brightcove_section,
-		array(
-			'ucf_com_shortcodes_brightcove_default_height',
-			'Default video height (in pixels)'
-		)
-	);
-	register_setting(
-		$settings_page,
-		'ucf_com_shortcodes_brightcove_default_height'
-	);
-	add_settings_field(
-		'ucf_com_shortcodes_brightcove_default_width',
-		'Default width',
-		'ucf_com_shortcodes_input_text',
-		$settings_page,
-		$brightcove_section,
-		array(
-			'ucf_com_shortcodes_brightcove_default_width',
-			'Default video width (in pixels)'
-		)
-	);
-	register_setting(
-		$settings_page,
-		'ucf_com_shortcodes_brightcove_default_width'
-	);
+	public function shortcode_brightcove_function( $attrs ) {
 
-
-}
-
-function brightcove_options_callback() {
-	echo '<p>Set the defaults for Brightcove videos</p>';
-}
-
-function promo_video_options_callback() {
-	echo '<p>Set the defaults for Promo Video</p>';
-}
-
-function ucf_com_shortcodes_input_text( $args ) {
-	// Note the ID and the name attribute of the element should match that of the ID in the call to add_settings_field
-	$html = '<input type="text" id="' . $args[ 0 ] . '" name="' . $args[ 0 ] . '" value="' . get_option( $args[ 0 ] ) . '"/>';
-
-	// Here, we will take the first argument of the array and add it to a label next to the input
-	$html .= '<label for="' . $args[ 0 ] . '"> ' . $args[ 1 ] . '</label>';
-	echo $html;
-}
-
-/**
- * base_url Short Code
- * This returns the base url of the current site.
- *
- */
-add_shortcode( 'shortcode', 'base_url' );
-function base_url( $attrs ) {
-	extract( shortcode_atts( array(
-		                         'attribute' => 'default parameter'
-	                         ), $attrs ) );
-
-	return get_bloginfo( 'url' );
-}
-
-/**
- * brightcove Short Code
- * This places an inline video on the page. The video is hosted on Brightcove.com.
- */
-add_shortcode( 'brightcove', 'bc_func' );
-function bc_func( $attrs ) {
-
-	return '<div style="display:none"></div>
+		return '<div style="display:none"></div>
 
 	<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
 
@@ -262,45 +258,67 @@ function bc_func( $attrs ) {
 
 	<script type="text/javascript">brightcove.createExperiences();</script>';
 
+	}
+
+	/**
+	 * base_url Short Code
+	 * Replaces the 'base_url' keyword with the actual HTML desired and returns it to wordpress.
+	 * This returns the base url of the current site. That way, a user can create an absolute
+	 * link without knowing the current domain.
+	 *
+	 * @param array $attrs Attributes are passed by wordpress automatically.
+	 *
+	 * @return string
+	 *
+	 */
+	public function shortcode_base_url_function( $attrs ) {
+		extract( shortcode_atts( array(
+			                         'attribute' => 'default parameter'
+		                         ), $attrs ) );
+
+		return get_bloginfo( 'url' );
+	}
+
+	/**
+	 * Include this plugin's javascript file.
+	 *
+	 * @param $plugin_array
+	 *
+	 * @return mixed
+	 */
+	public function tinymce_brightcove_js( $plugin_array ) {
+		$plugin_array[ 'ucf_com_brightcove' ] = plugins_url( '/plugin.js', __FILE__ ); // include the javascript for the button, located inside the current plugin folder
+		return $plugin_array;
+	}
+
+	/**
+	 * Include this plugin's css file.
+	 *
+	 * @param $mce_css
+	 *
+	 * @return mixed
+	 */
+	public function tinymce_brightcove_css( $mce_css ) {
+		wp_register_style( 'ucf_com_brightcove_css_file', plugins_url( 'style.css', __FILE__ ) );
+		wp_enqueue_style( 'ucf_com_brightcove_css_file' );
+
+		return $mce_css;
+
+	}
+
+	/**
+	 * Add the buttons on the tinymce interface
+	 *
+	 * @param $buttons
+	 *
+	 * @return mixed
+	 */
+	public function tinymce_brightcove_button( $buttons ) {
+		array_push( $buttons, 'separator', 'ucf_com_brightcove_key' );
+
+		return $buttons;
+	}
+
 }
 
-
-/**
- * TinyMCE Buttons
- */
-//add_action( 'admin_head', 'ucf_com_tinymce');	// only add these filters with admin pages
-//function ucf_com_tinymce(){
-add_filter( 'mce_external_plugins', 'ucf_com_tinymce_brightcove_js' );
-add_filter( 'mce_css', 'ucf_com_tinymce_brightcove_css' );
-add_filter( 'mce_buttons', 'ucf_com_tinymce_brightcove_button' );
-//}
-function ucf_com_tinymce_brightcove_js( $plugin_array ) {
-	$plugin_array[ 'ucf_com_brightcove' ] = plugins_url( '/plugin.js', __FILE__ ); // include the javascript for the button, located inside the current plugin folder
-	return $plugin_array;
-}
-
-function ucf_com_tinymce_brightcove_css( $mce_css ) {
-	wp_register_style( 'ucf_com_brightcove_css_file', plugins_url( 'style.css', __FILE__ ) );
-	wp_enqueue_style( 'ucf_com_brightcove_css_file' );
-
-	/*
-	  
-	if ( ! empty( $mce_css ) )
-		$mce_css .= ',';
-
-	$mce_css .= plugins_url( 'style.css', __FILE__ );
-	
-	
-	*/
-
-	return $mce_css;
-
-}
-
-function ucf_com_tinymce_brightcove_button( $buttons ) {
-	array_push( $buttons, 'separator', 'ucf_com_brightcove_key' );
-
-	return $buttons;
-}
-
-?>
+$ucf_com_shortcodes_settings_object = new ucf_com_shortcodes_settings();
