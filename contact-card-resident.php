8fde261c4e057485d6a0a8dc9936a5c767d18254
Stephen Schrauger <stephen@schrauger.com>
2016-08-26 15:21:32 -0400

    only show fields if defined

--- contact-card-resident.php
+++ contact-card-resident.php
@@ -12,10 +12,20 @@ $image = $preview[0];
     <a href="<?php the_permalink(); ?>" class="contact-image" <?php if ( get_field( 'photo' ) ) { ?> style="background: url(' <?php echo $image; ?>') no-repeat center center; background-size: cover;" <?php } else { ?> style="background: #fff url('<?php bloginfo( 'template_url' ); ?>/images/sprites/main-med-sprite.png') no-repeat 24px -413px;" }  <?php } ?> > </a>
 
     <ul class="contact-info">
-        <li><span>Medical School:</span> <?php the_field( 'res_medical_school' ); ?></li>
-        <li><span>Resident Year:</span> <?php the_field( 'res_resident_year' ); ?></li>
-        <li><span>Career Interest:</span> <?php the_field( 'res_career_interest' ); ?></li>
-        <li><span>Fun Fact:</span> <?php echo the_field( 'res_fun_fact' ); ?></li>
+        <?php
+        if ($res_medical_school = get_field( 'res_medical_school' )){
+            echo "<li><span>Medical School:</span>$res_medical_school</li>";
+        }
+        if ($res_resident_year = get_field( 'res_resident_year' )){
+            echo "<li><span>Resident Year:</span>$res_resident_year</li>";
+        }
+        if ($res_career_interest = get_field( 'res_career_interest' )){
+            echo "<li><span>Career Interest:</span>$res_career_interest</li>";
+        }
+        if ($res_fun_fact = get_field( 'res_fun_fact' )){
+            echo "<li><span>Fun Fact:</span>$res_fun_fact</li>";
+        }
+        ?>
     </ul>
 
 </aside>
