176d275bb8cc1cdee045d595e215dd6543c3552d
Stephen Schrauger <stephen@schrauger.com>
2016-08-29 15:19:45 -0400

    adds spaces after labels

--- contact-card-resident.php
+++ contact-card-resident.php
@@ -14,22 +14,22 @@ $image = $preview[0];
     <ul class="contact-info">
         <?php
         if ($res_clinical_site = get_field( 'res_clinical_site' )){
-            echo "<li><span>Clinical Site:</span>$res_clinical_site</li>";
+            echo "<li><span>Clinical Site: </span>$res_clinical_site</li>";
         }
         if ($res_medical_school = get_field( 'res_medical_school' )){
-            echo "<li><span>Medical School:</span>$res_medical_school</li>";
+            echo "<li><span>Medical School: </span>$res_medical_school</li>";
         }
         if ($res_residency = get_field( 'res_residency' )){
-            echo "<li><span>Residency:</span>$res_residency</li>";
+            echo "<li><span>Residency: </span>$res_residency</li>";
         }
         if ($res_resident_year = get_field( 'res_resident_year' )){
-            echo "<li><span>Resident Year:</span>$res_resident_year</li>";
+            echo "<li><span>Resident Year: </span>$res_resident_year</li>";
         }
         if ($res_career_interest = get_field( 'res_career_interest' )){
-            echo "<li><span>Career Interest:</span>$res_career_interest</li>";
+            echo "<li><span>Career Interest: </span>$res_career_interest</li>";
         }
         if ($res_fun_fact = get_field( 'res_fun_fact' )){
-            echo "<li><span>Fun Fact:</span>$res_fun_fact</li>";
+            echo "<li><span>Fun Fact: </span>$res_fun_fact</li>";
         }
         ?>
     </ul>
